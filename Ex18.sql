ffe6780e40a6aca7f27b9f14bbb59f85305cbc9f
Lucas Mercena <129337756+lucasmercena@users.noreply.github.com>
2024-04-29 21:51:25 -0300

    Delete Ex18.sql

--- Ex18.sql
+++
@@ -1,6 +0,0 @@
--- 18. Faça uma relação geral de todos os associados e planos que eles possuem. A relação deve ser apresentada em ordem 
---     ascendente pelo campo tipo de plano e descendente pelo campo de identificação do nome do associado.
-SELECT * FROM ASSOCIADOS
-JOIN PLANOS
-ON ASSOCIADOS.PLANO = PLANOS.NUMERO
-ORDER BY ASSOCIADOS.PLANO ASC, ASSOCIADOS.NOME DESC;
