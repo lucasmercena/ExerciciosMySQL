772cf42973b30b54cce90217803a87f3e0608930
Lucas Mercena <129337756+lucasmercena@users.noreply.github.com>
2024-04-29 21:51:40 -0300

    Delete Ex17.sql

--- Ex17.sql
+++
@@ -1,5 +0,0 @@
--- 17. Apresente uma listagem completa de todos os campos de ambas as tabelas ordenados por tipo de plano.
-SELECT * FROM ASSOCIADOS
-JOIN PLANOS
-ON ASSOCIADOS.PLANO = PLANOS.NUMERO
-ORDER BY DESCRICAO;
