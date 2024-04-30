554387d90c8ed292e4cfac25fd591c9d680a8ffb
Lucas Mercena <129337756+lucasmercena@users.noreply.github.com>
2024-04-29 21:54:52 -0300

    Delete Ex08.sql

--- Ex08.sql
+++
@@ -1,4 +0,0 @@
--- 8. Apresente uma relação completa de todos os campos de ambas as tabelas em que o associado possua SILVA no nome.
-SELECT * FROM PLANOS
-INNER JOIN ASSOCIADOS
-ON ASSOCIADOS.PLANO = PLANOS.NUMERO;
